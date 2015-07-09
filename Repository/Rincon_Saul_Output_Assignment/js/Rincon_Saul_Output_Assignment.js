e1b773d74f907c1c8ca349a9050c59df67902fac
SRincon <recon8639@fullsail.edu>
2015-07-09 09:09:53 -0700

    More variable
    Mixture of number, string and booleans

--- Repository/Rincon_Saul_Output_Assignment/js/Rincon_Saul_Output_Assignment.js
+++ Repository/Rincon_Saul_Output_Assignment/js/Rincon_Saul_Output_Assignment.js
@@ -12,3 +12,12 @@ var isFirefighter=false;
 console.log(isFirefighter);//Boolean is obtained
 var phrase="I was in the United States Army Infantry from 2003 through 2006";//String variable
 console.log(phrase);
+
+var isPoliceofficer=true;
+console.log(isPoliceofficer);//Boolean
+
+var myKids=3;
+console.log(myKids);//Number variable
+
+var yearsofWisdom=30;
+console.log(yearsofWisdom + " years of it");//String variable
