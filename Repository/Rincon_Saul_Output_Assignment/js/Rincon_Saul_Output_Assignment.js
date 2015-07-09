c27619d5e68e1bedf601e64c08fb9241ab350f6e
SRincon <recon8639@fullsail.edu>
2015-07-09 09:07:36 -0700

    Var phrase
    String Variable

--- Repository/Rincon_Saul_Output_Assignment/js/Rincon_Saul_Output_Assignment.js
+++ Repository/Rincon_Saul_Output_Assignment/js/Rincon_Saul_Output_Assignment.js
@@ -10,3 +10,5 @@ var myJob=8;
 console.log(myJob + " years");//Number Variable
 var isFirefighter=false;
 console.log(isFirefighter);//Boolean is obtained
+var phrase="I was in the United States Army Infantry from 2003 through 2006";//String variable
+console.log(phrase);
