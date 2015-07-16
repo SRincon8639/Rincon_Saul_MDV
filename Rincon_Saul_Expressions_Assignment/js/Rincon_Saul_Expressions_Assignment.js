bb4adcd6cf0f04d34becceba7640e0f5638dc338
SRincon <recon8639@fullsail.edu>
2015-07-16 16:29:32 -0700

    Revert "Variables plus prompts"
    This reverts commit 3b8d9e20cd6fd12890eb1cb81806bf91f9cf7e61.

--- Rincon_Saul_Expressions_Assignment/js/Rincon_Saul_Expressions_Assignment.js
+++ Rincon_Saul_Expressions_Assignment/js/Rincon_Saul_Expressions_Assignment.js
@@ -4,15 +4,5 @@ SDI 1507
 Expressions Assignment
 07/16/2015
 */
-//alert("hi");
-
 
 //Variables
-
-var myGames;
-var oneMoreGame=myGames+=1;
-var oneLessGame=myGames-=1;
-
-myGames=prompt("Enter how many games you have?");
-oneMoreGame=prompt("I have " + " " + myGames +" " +"now, but If I buy one more, I will have" + "!");
-oneLessGame=prompt("I have " + " " + myGames + " " + "now, but If I sell one, I will have" + "!");
