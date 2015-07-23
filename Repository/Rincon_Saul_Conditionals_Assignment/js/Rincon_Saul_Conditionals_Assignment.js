06f6ca9e8a88b2fbd66b4481c00e74f938f3899a
SRincon <recon8639@fullsail.edu>
2015-07-23 15:00:59 -0700

    First conditional and if/else statement trial

--- Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
+++ Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
@@ -18,10 +18,40 @@ alert("You have 200 rounds but need 500 to attend training.");
 console.log("I currently have"+" " + currentAmmo +" "+"rounds to attend handgun training" + ".");
 console.log("I need " + " "+ ammoRequired+ " "+ "rounds to attemd the handgun training"+ ".");
 console.log("Since I am short on ammunition, I need" +" "+ (ammoRequired-currentAmmo)+" "+ "more to meet the requirement" + ".");
-console.log("It is"+" " + noAmmoLeft +" "+ "that there isn't ammo"+".");
+console.log("It is"+" " + noAmmo +" "+ "that I don't have enough ammuniton to attend training "+".");
+
+noAmmoLeft=confirm("Do you have the ammuniton to attend? (Ok for Yes, Cancel for No)");
+
+if(noAmmo===true){
+	prompt("How many rounds do you have total?");
+	console.log("200 rounds");
+
+if(ammoToBuy<=500){
+	ammoToBuy=ammoToBuy+currentAmmo;
+	console.log(ammoToBuy+" "+ "rounds is the total");
+}
+else{
+	console.log("We can now attend the training!");
+	}
+}
+else{
+	console.log(" We can attend the training due to"+" "+ extraRoundsFound+ " "+"rounds found in closet");
+	var emptyString=prompt("How many rounds were found");
+
+	if(emptyString===""){
+		emptyString=prompt("Enter the rounds that were found in your closet NOW!!")
+		console.log("You can train with"+" "+ emptyString+".");
+	}
+
+else{
+	emptyString=Number("500");
+	console.log("Finally, lets go training with"+" "+ emptyString+".");
+	}
+}
 
 
 
+	
 
 
 
