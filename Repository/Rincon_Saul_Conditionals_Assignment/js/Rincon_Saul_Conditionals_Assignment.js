d168f9b25951c2e2530008a6aaedf7abeb2fdccb
SRincon <recon8639@fullsail.edu>
2015-07-23 13:20:09 -0700

    First set of Outputs

--- Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
+++ Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
@@ -11,7 +11,14 @@ var currentAmmo=200;
 var ammoRequired=500;
 var ammoToBuy=300;
 var noAmmoLeft=false;
-var extraAmmo=200;
+//var extraAmmo=200;
+
+//Outputs
+alert("You have 200 rounds but need 500 to attend training.");
+console.log("I currently have"+" " + currentAmmo +" "+"rounds to attend handgun training" + ".");
+console.log("I need " + " "+ ammoRequired+ " "+ "rounds to attemd the handgun training"+ ".");
+console.log("Since I am short on ammunition, I need" +" "+ (ammoRequired-currentAmmo)+" "+ "more to meet the requirement" + ".");
+console.log("It is"+" " + noAmmoLeft +" "+ "that there isn't ammo"+".");
 
 
 
