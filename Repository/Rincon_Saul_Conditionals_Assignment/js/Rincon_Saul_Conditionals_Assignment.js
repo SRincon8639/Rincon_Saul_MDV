89fa017ad1f02081ce3d9105a822d1f34a4e4af4
SRincon <recon8639@fullsail.edu>
2015-07-23 16:09:51 -0700

    trial and error for conditional

--- Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
+++ Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
@@ -6,46 +6,55 @@ SDI Conditional Assignment
 */
 //alert("hi");
 
-// Variables				
+// Variables				//These variables will be used in determining if we go training if we buy 300 more rounds
 var currentAmmo=200;
 var ammoRequired=500;
 var ammoToBuy=300;
-var noAmmoLeft=false;
-//var extraAmmo=200;
+var noAmmo=false;			//Boolean
+
+
 
 //Outputs
-alert("You have 200 rounds but need 500 to attend training.");
-console.log("I currently have"+" " + currentAmmo +" "+"rounds to attend handgun training" + ".");
-console.log("I need " + " "+ ammoRequired+ " "+ "rounds to attemd the handgun training"+ ".");
+alert("You have 200 rounds but need 500 to attend training.");//This alert will let user know the amounts of rounds he has
+console.log("I currently have"+" " + currentAmmo +" "+"rounds to attend handgun training" + ".");//All the console logs below will address to the user what he needs to add to make calculation work
+console.log("I need " + " "+ ammoRequired+ " "+ "rounds to attend the handgun training"+ ".");
 console.log("Since I am short on ammunition, I need" +" "+ (ammoRequired-currentAmmo)+" "+ "more to meet the requirement" + ".");
 console.log("It is"+" " + noAmmo +" "+ "that I don't have enough ammuniton to attend training "+".");
 
-noAmmoLeft=confirm("Do you have the ammuniton to attend? (Ok for Yes, Cancel for No)");
+noAmmoLeft=confirm("Do you have the ammuniton to attend? (Ok for Yes, Cancel for No)");//This confirm is used to dictate whether you have the bullets to train or not
 
-if(noAmmo===true){
+if(noAmmo===false){
 	prompt("How many rounds do you have total?");
-	console.log("200 rounds");
+	
 
-if(ammoToBuy<=500){
-	ammoToBuy=ammoToBuy+currentAmmo;
-	console.log(ammoToBuy+" "+ "rounds is the total");
+if(ammoRequired===500){
+	console.log("Lets go shoot!");
 }
-else{
+else if(ammoRequired>500){
+	console.log("You cant train");
+
+}
+else {
 	console.log("We can now attend the training!");
 	}
 }
-else{
-	console.log(" We can attend the training due to"+" "+ extraRoundsFound+ " "+"rounds found in closet");
-	var emptyString=prompt("How many rounds were found");
+else {
+	console.log(" We can attend the training due to"+" "+ ammoToBuy+ " "+"extra rounds bought");
+	//var emptyString=prompt("How many rounds were bought");
+
 
 	if(emptyString===""){
-		emptyString=prompt("Enter the rounds that were found in your closet NOW!!")
-		console.log("You can train with"+" "+ emptyString+".");
+		emptyString=prompt("Enter the rounds that were bought NOW!!")
+		
+
+		if(""<500){
+			console.log("You cant train if you dont have the rounds");
+		}
 	}
 
 else{
-	emptyString=Number("500");
-	console.log("Finally, lets go training with"+" "+ emptyString+".");
+	emptyString=("");
+	console.log("Finally, lets go training with"+" "+ emptyString+" "+"rounds"+".");
 	}
 }
 
