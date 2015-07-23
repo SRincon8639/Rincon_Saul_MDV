2f2374ee5d1e954deeaaf7ce1884e8b0a3e6e7ed
SRincon <recon8639@fullsail.edu>
2015-07-23 12:52:18 -0700

    Html and Start of Assignment with Variables

---
+++ Repository/Rincon_Saul_Conditionals_Assignment/js/Rincon_Saul_Conditionals_Assignment.js
@@ -0,0 +1,20 @@
+/*
+Saul Rincon
+SDI Conditional Assignment
+1507
+07/23/2015
+*/
+//alert("hi");
+
+// Variables				
+var currentAmmo=200;
+var ammoRequired=500;
+var ammoToBuy=300;
+var noAmmoLeft=false;
+var extraAmmo=200;
+
+
+
+
+
+
