5cf7f62c574f6e7a4fbbcc297bf90c6ec1fb5ecd
SRincon <recon8639@fullsail.edu>
2015-07-30 20:55:33 -0700

    final...couldnt get it to work

--- Repository/Rincon_Saul_Functions_Assignment/js/Rincon_Saul_Functions_Assignment.js
+++ Repository/Rincon_Saul_Functions_Assignment/js/Rincon_Saul_Functions_Assignment.js
@@ -8,44 +8,63 @@ Functions
 //alert("hi!");
 
 
-// I will use the Florida Lottery or Powerball. This will prompt user what they want to use
 
-//confirm("Do you want to participate in the Florida Lottery or Powerball? (Ok for Lotto, Cancel for Powerball)");
+
+confirm("Do you want to participate in the Florida Lottery or Powerball? (Ok for Lotto, Cancel for Powerball)");
+
 
 
 //Variables
 var numPicked;
 var lottery=false;
-var ballChosen;
 
-//Function
-function LotteryCheck(){
+//Functions
+function Start(){
+	result();
+
+}
+
+function lotteryCheck(){
 
 //This is section for lottery
 
-if(lottery===true){
+if(lottery==true){
 	numPicked=6;
+	ballGenerator(1,53);//
 }
 //This section is for Powerball
 else{
-	numPicked===5;
+	numPicked=5;
+	ballGenerator(1,59);
+	var ballChosen=Math.random()*(35-1)+1;
+	//ballArray[6]=Math.round(ballChosen);
 	}
 }
 
-
+//This function will create the random number generator
 function ballGenerator(min, max){
 
-	var ballArray=[];
-for (var i=0; i<numPicked;i++){
+	var ballArray=[];//
+	for (var i=0; i<numPicked;i++){
 	
-	var ballChosen=Math.random()*(max-min)+min;
-	ballArray[i]=Math.round(ballChosen);
+	var ballChosen=Math.random()*(max-min)+min;//This will pick a random number between min and max
+	ballArray[i]=Math.round(ballChosen);//This rounds the previous random number to a whole number
 
 }
 
-	return ballArray;
+	return ballArray;//
 
 }
+
+
+	function result(){
+	lotteryCheck();
+	console.log(lotteryCheck);
+	
+}
+	
+	
+	
 
 
 
