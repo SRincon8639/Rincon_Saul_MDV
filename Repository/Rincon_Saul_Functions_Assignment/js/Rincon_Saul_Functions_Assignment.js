6e261774b53f9d971c45b0ac791bc2d6cea51053
SRincon <recon8639@fullsail.edu>
2015-07-30 19:36:23 -0700

    function for ballGenerator

--- Repository/Rincon_Saul_Functions_Assignment/js/Rincon_Saul_Functions_Assignment.js
+++ Repository/Rincon_Saul_Functions_Assignment/js/Rincon_Saul_Functions_Assignment.js
@@ -16,21 +16,34 @@ Functions
 //Variables
 var numPicked;
 var lottery=false;
+var ballChosen;
 
 //Function
 function LotteryCheck(){
 
 //This is section for lottery
 
-if(lottery=true){
+if(lottery===true){
 	numPicked=6;
 }
 //This section is for Powerball
 else{
-	numPicked=5;
+	numPicked===5;
 	}
 }
+
+
+function ballGenerator(min, max){
+
+	var ballArray=[];
 for (var i=0; i<numPicked;i++){
+	
+	var ballChosen=Math.random()*(max-min)+min;
+	ballArray[i]=Math.round(ballChosen);
+
+}
+
+	return ballArray;
 
 }
 
@@ -61,29 +74,4 @@ for (var i=0; i<numPicked;i++){
 
 
 
-
-
-
-
-
-
-/*var Florida Lottery= {
-	max: 53;
-	min:1;
-	num drawn:6;
-
-}
-
-var Powerball Numbers={
-	max:59;
-	min:1;
-	num drawn:5
-}
-
-var Powerball={
-	max:35;
-	min:1;
-	drawn:1;
-}
-*/
 
