efe856f5769fe2d3f9b79e1175bf29ed3b8593fe
SRincon <recon8639@fullsail.edu>
2015-07-30 19:15:27 -0700

    created variables and if/else

--- Repository/Rincon_Saul_Functions_Assignment/js/Rincon_Saul_Functions_Assignment.js
+++ Repository/Rincon_Saul_Functions_Assignment/js/Rincon_Saul_Functions_Assignment.js
@@ -5,4 +5,85 @@ Functions
 07/20/2015
 */
 
-alert("hi!");
+//alert("hi!");
+
+
+// I will use the Florida Lottery or Powerball. This will prompt user what they want to use
+
+//confirm("Do you want to participate in the Florida Lottery or Powerball? (Ok for Lotto, Cancel for Powerball)");
+
+
+//Variables
+var numPicked;
+var lottery=false;
+
+//Function
+function LotteryCheck(){
+
+//This is section for lottery
+
+if(lottery=true){
+	numPicked=6;
+}
+//This section is for Powerball
+else{
+	numPicked=5;
+	}
+}
+for (var i=0; i<numPicked;i++){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var Florida Lottery= {
+	max: 53;
+	min:1;
+	num drawn:6;
+
+}
+
+var Powerball Numbers={
+	max:59;
+	min:1;
+	num drawn:5
+}
+
+var Powerball={
+	max:35;
+	min:1;
+	drawn:1;
+}
+*/
+
